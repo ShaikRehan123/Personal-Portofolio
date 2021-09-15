2cadc0f3879075886171eb21536ab83390a88a0d
RehantheCoder <codinghub41@gmail.com>
2021-09-15 21:00:15 +0530

    Second commit

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -46,7 +46,7 @@ const Timeline = () => {
     <Section id="about">
       <SectionTitle>About Me</SectionTitle>
       <SectionText>
-      The purpose of JavaScript Mastery is to help aspiring and established developers to take their development skills to the next level and build awesome apps.
+      An Newly Developer Interested In Web Development
       </SectionText>
       <CarouselContainer ref={carouselRef} onScroll={handleScroll}>
         <>
